33c501b57b8af6a5d78f457bceedc47397f29d06
ODOT-Bjorn <Bjorn.Miller@ODOT.Oregon.gov>
2025-05-05 15:07:12 -0700

    Update streamlit_app.py
    added API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,8 @@ if ingredients_list:
         #if ingredients_string:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order+'!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
